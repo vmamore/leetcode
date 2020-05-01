6c00b62b777b1e2e35ef5179e37673d35edab4fa
unknown <vinicius.mamore@gmail.com>
2020-05-01 17:25:43 -0400

    solution find for defangig ip address

---
+++ defanging-ip-address/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace defanging_ip_address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DefangIPaddr("1.1.1.1"));
+        }
+
+        public static string DefangIPaddr(string address) {
+            var addressSplittedByDot = address.Split('.');
+            return string.Join("[.]", addressSplittedByDot);
+        }   
+    }
+}
