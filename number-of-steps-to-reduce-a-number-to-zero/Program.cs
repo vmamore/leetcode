25c7c12d6786d9c8417cf35b21144f1d59a4aeb1
unknown <vinicius.mamore@gmail.com>
2020-05-11 22:43:12 -0400

    resolution of number of steps

---
+++ number-of-steps-to-reduce-a-number-to-zero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace number_of_steps_to_reduce_a_number_to_zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int NumberOfSteps(int num)
+        {
+            int output = 0;
+            do
+            {
+                if (num % 2 == 0)
+                {
+                    output++;
+                    num = num / 2;
+                }
+                else
+                {
+                    output++;
+                    num = num - 1;
+                }
+            } while (num != 0);
+
+            return output;
+        }
+
+    }
+}
